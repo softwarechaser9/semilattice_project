5bffb914cbbd21e1f13eb880cb103a468b8aebd4
limonzap <limon@zapsell.ch>
2025-09-06 11:39:09 +0600

    Add build script for Render deployment with migrations

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Build script for Render deployment
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run database migrations
+python manage.py migrate --noinput
+
+# Collect static files
+python manage.py collectstatic --noinput --clear
